b79fbcbb7ed1f1b559475bb64b87207ff9573597
TanviGarg13 <gargtanvi12345@gmail.com>
2023-08-17 09:19:06 +0530

    first commit

---
+++ scripts/word-list.js
@@ -0,0 +1,81 @@
+const wordList = [
+    {
+        word:"panipuri",
+        hint:"A popular street food consisting of hollow puris filled with spiced water, potatoes, and chutney."
+    }, 
+
+    {
+        word: "samosa",
+        hint: " A triangular pastry filled with spiced potatoes & peas."
+    },
+
+    {
+        word:"dhokla",
+        hint: " A steamed and spongy cake made from fermented rice and chickpea flour."
+    },
+
+    {
+        word: "chaat",
+        hint: " A popular street food with a mix of savory ingredients like chutneys, spices, and fried snacks."
+    },
+
+    {
+        word: "dosa",
+        hint: "A thin, crispy Indian pancake made from fermented rice and lentil batter."
+    },
+
+    {
+        word: "biryani",
+        hint: "A flavorful rice dish with spices and meat or vegetables."
+    }, 
+
+    {
+        word: "misalpav",
+        hint: "A flavorful dish made with sprouted lentils, spices, and topped with crunchy snacks."
+    },
+
+    {
+        word: "rasgulla",
+        hint: "Soft and spongy sweet dumplings made from chhena (Indian cottage cheese) soaked in sugar syrup."
+    },
+
+    {
+        word: "shrikhand",
+        hint: "A creamy dessert made from strained yogurt, often flavored with saffron and cardamom."
+    },
+
+    {
+        word: "kachori",
+        hint: "Deep-fried pastry filled with spiced lentils, peas, or potatoes."
+    },
+
+    {
+        word: "ghevar",
+        hint: "A traditional Rajasthani dessert made from deep-fried batter, often soaked in sugar syrup."
+    },
+
+    {
+        word: "dalbaatichurma",
+        hint: "A classic Rajasthani dish consisting of lentil curry (dal) served with baked wheat flour balls and crushed sweetened bread."
+    },
+
+    {
+        word: "gattekisabzi",
+        hint: "A curry made with gram flour (besan) dumplings cooked in a spiced yogurt gravy."
+    },
+
+    {
+        word: "rajasthanikadi",
+        hint: "A spiced yogurt-based curry often paired with deep-fried fritters (pakoras)."
+    },
+
+    {
+        word: "churma", 
+        hint: "A dessert made from crushed wheat flour balls combined with ghee and sugar."
+    },
+
+    {
+        word: "rajmachawal",
+        hint: " A comforting dish of red kidney beans cooked in a spiced tomato gravy, served with rice."
+    }
+];
